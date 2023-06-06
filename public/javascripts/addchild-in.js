b9c3aadbc6247e1db8f5b0e254570a93d0b14695
wayne-0315 <80502713+wayne-0315@users.noreply.github.com>
2023-06-07 04:26:34 +0800

    Update addchild-in.js

--- public/javascripts/addchild-in.js
+++ public/javascripts/addchild-in.js
@@ -20,27 +20,27 @@ function addchild() {
         
     }
 
-//     var img = document.getElementById('u_img_file');
+    var img = document.getElementById('u_img_file');
     
-//     var fromData = new FormData();
-//     fromData.append('file', img.files[0]);
-//     var url = "/upload1?id=" + $.cookie('userID');
-//     $.ajax({
-//         url: url,
-//         type: "POST",
-//         data: fromData,
-//         processData: false,
-//         contentType: false,
-//         success: function (res) {
-//             if (res.status == 0) {
-//                 //alert("上傳成功");
-//                 //history.go(0);
-//             }
-//         },
-//         error: function (err) {
-//             console.log(err);
-//         }
-//     });
+    var fromData = new FormData();
+    fromData.append('file', img.files[0]);
+    var url = "/upload1?id=" + $.cookie('userID');
+    $.ajax({
+        url: url,
+        type: "POST",
+        data: fromData,
+        processData: false,
+        contentType: false,
+        success: function (res) {
+            if (res.status == 0) {
+                //alert("上傳成功");
+                //history.go(0);
+            }
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
 
     $.post("/childupdate", postdata, function (res) {
         if (res.status == 0) {
