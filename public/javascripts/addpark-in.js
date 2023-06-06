362dd7ff812f3b7e387f74b247897fcc1add0b65
wayne-0315 <80502713+wayne-0315@users.noreply.github.com>
2023-06-07 04:28:35 +0800

    Update addpark-in.js

--- public/javascripts/addpark-in.js
+++ public/javascripts/addpark-in.js
@@ -15,30 +15,30 @@ function addpark() {
         aaa: $('#aaa').val(),
     }
 
-//     var img = document.getElementById('u_img_file');
-//     if (!/.(gif|jpg|jpeg|png|GIF|JPG|PNG|JPEG)$/.test(img.value)) {
-//         alert("圖片類型不正確");
-//         return;
-//     }
-//     var fromData = new FormData();
-//     fromData.append('file', img.files[0]);
-//     var url = "/upload?id=" + $.cookie('userID');
-//     $.ajax({
-//         url: url,
-//         type: "POST",
-//         data: fromData,
-//         processData: false,
-//         contentType: false,
-//         success: function (res) {
-//             if (res.status == 0) {
-//                 //alert("上傳成功");
-//                 //history.go(0);
-//             }
-//         },
-//         error: function (err) {
-//             console.log(err);
-//         }
-//     });
+    var img = document.getElementById('u_img_file');
+    if (!/.(gif|jpg|jpeg|png|GIF|JPG|PNG|JPEG)$/.test(img.value)) {
+        alert("圖片類型不正確");
+        return;
+    }
+    var fromData = new FormData();
+    fromData.append('file', img.files[0]);
+    var url = "/upload?id=" + $.cookie('userID');
+    $.ajax({
+        url: url,
+        type: "POST",
+        data: fromData,
+        processData: false,
+        contentType: false,
+        success: function (res) {
+            if (res.status == 0) {
+                //alert("上傳成功");
+                //history.go(0);
+            }
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
 
     $.post("/parkupdate", postdata, function (res) {
         if (res.status == 0) {
