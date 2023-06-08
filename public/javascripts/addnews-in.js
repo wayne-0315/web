c8074cd20b97bf76186c27e15d052a9ca0979d79
wayne-0315 <80502713+wayne-0315@users.noreply.github.com>
2023-06-09 04:46:40 +0800

    Update addnews-in.js

--- public/javascripts/addnews-in.js
+++ public/javascripts/addnews-in.js
@@ -35,7 +35,7 @@ function addnews() {
 //             console.log('---33 receving response');
             if (res.status == 0) {
 //                 console.log('---33 upload success');
-                alert("上傳成功");
+//                 alert("上傳成功");
 //                 location.href = '/news';
                 //history.go(0);
             }
